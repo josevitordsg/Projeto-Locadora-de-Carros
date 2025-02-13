053775ff3c6e8eb4e9ec14c6169b24fba943031e
José Vitor <josevitorgeni@gmail.com>
2025-02-13 17:05:47 -0300

    Delete locadora_backend directory

--- locadora_backend/usuarios/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.urls import path
-
-urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
